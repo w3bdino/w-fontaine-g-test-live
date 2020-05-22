96214f4830ad885000e1f4eae4bda4b0a80d19ca
Dino Robles <webdino.r@gmail.com>
2020-05-22 11:36:22 +0800

    Updates

---
+++ precache-manifest.8fac1eb7a764bf4eb7c0a07ed702f109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e4cb5c040638fe392651dd3a820254",
+    "url": "/w-fontaine-g-test-live/index.html"
+  },
+  {
+    "revision": "503a6b3d5eac2c02fc86",
+    "url": "/w-fontaine-g-test-live/static/css/main.b4db97af.chunk.css"
+  },
+  {
+    "revision": "5758fce8884aac6697ee",
+    "url": "/w-fontaine-g-test-live/static/js/2.54fff793.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/w-fontaine-g-test-live/static/js/2.54fff793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503a6b3d5eac2c02fc86",
+    "url": "/w-fontaine-g-test-live/static/js/main.39d327f6.chunk.js"
+  },
+  {
+    "revision": "baaf3884cb98c26fe29d",
+    "url": "/w-fontaine-g-test-live/static/js/runtime-main.1ef2da3c.js"
+  }
+]);
